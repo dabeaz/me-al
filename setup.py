c6d85d9c72fb8805114e0f40e01b113a7e9d59f7
David Beazley <dave@dabeaz.com>
2015-03-30 00:19:50 +0000

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='meẗal',
+      version='1.0',
+      description='Meẗal - Better Than Decoration',
+      author='David Beazley',
+      author_email='dave@dabeaz.com',
+      url='https://github.com/dabeaz/me-al',
+      py_modules=['meẗal'],
+     )
