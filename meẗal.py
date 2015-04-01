4844922359d18fe693a46625ed4b8815f1a16592
David Beazley <dave@dabeaz.com>
2015-04-01 09:16:33 +0000

    Minor markdown edits

--- meẗal.py
+++ meẗal.py
@@ -1,11 +1,13 @@
 '''
 Meẗal - The Decorator
+=====================
 
 Synopsis
-========
+--------
 So, you've fully explored the use of decorators and maybe even
 decorators with arguments.  Where do you go from there?  Where you
-ask?  You, my friend, need to apply Meẗal--the better decorator.
+ask?  You, my friend, need to apply Meẗal--as in 
+[Heavy Metal](http://en.wikipedia.org/wiki/Metal_umlaut).
 
 For example, here is a function:
 
@@ -50,7 +52,7 @@ Now, run your program:
 You'll see your decorator applied to the functions requesting
 meẗal. If the above command fails with an ImportError, you're probably
 using a Mac. That's an unfortunate choice as you can't really expect a
-machine like that to be used for serious tasks.  Nevertheless, if
+toy machine like that to be used for serious tasks.  Nevertheless, if
 you'd really like to apply meẗal anyways, you can still do it by
 changing the top level import to the following:
 
@@ -66,10 +68,10 @@ Although, for maximum portability, I'd suggest the following:
         meẗal = __import__('met\u0308al')
 
 Closer to the Meẗal
-===================
+-------------------
 Our simple example was really just a small taste to get the idea. If
 you really want to get serious though, you can use Meẗal with a
-package such as Numba:
+package such as [Numba](http://numba.pydata.org/):
 
     import meẗal
     from numba import jit
@@ -80,7 +82,7 @@ package such as Numba:
 Now, you're starting to get the idea.
 
 Meẗal is Different
-==================
+------------------
 Meẗal allows different decorators to be applied to the same module on
 different import statements--even in the same file!  Observe:
 
@@ -116,7 +118,7 @@ with different meẗal applied to it.  Try doing that with normal
 decorators!
 
 How it Works
-============
+------------
 When activated, Meẗal monitors all import statements in your program
 and looks for identifiers that include an metal umlaut (such names are
 said to be "meẗalized").  If found, those definitions are firstly
@@ -177,7 +179,7 @@ to different import statements and keep the resulting functions
 separate?  That question is easily answered by reading the source.
 
 Meẗal - Better than Explicit
-============================
+----------------------------
 Meẗal allows framework builders to explicitly indicate those functions
 that could be assisted with the addition of some meẗal.  However,
 unlike a normal decorator, it puts the power back into the hands of
@@ -189,17 +191,32 @@ easily apply the meẗal of their choice without worrying about
 others--no need for bikesheds here! Yes, the benefits are quite clear.
 
 Compatibility
-=============
+-------------
 Meẗal only works with Python 3.  If you love Python and you're still
 coding in Python 2, well, then fuck you.
 
 Limitations of Meẗal
-====================
+--------------------
 None are known or anticipated.
 
+Frequently "Asked" Questions
+----------------------------
+Q: How do you type the ẗ character?
+
+A: Using the ẗ key. WTF?
+
+Q: Can meẗal be installed using pip or easy_install?
+
+A: No.
+
+Q: Why is the Github repo called "me-al"?
+
+A: Reasons.
+
 Author
-======
-Meẗal is the creation of David Beazley (@dabeaz) who disavows all involvement.
+------
+Meẗal is the creation of David Beazley (@dabeaz) who officially
+disavows all involvement.
 '''
 
 import sys
